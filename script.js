296f957d2e32e65c74053183d7f076b874eca295
Star <mmdakane@rocketmail.com>
2025-11-05 22:51:12 +0200

    desktop view styling

---
+++ script.js
@@ -0,0 +1,11 @@
+const form = document.getElementById("contact-form");
+// const formData = new FormData(form);
+
+const fields = document.querySelectorAll('input[name]');
+const elements = form.elements;
+
+
+console.log(fields);
+console.log(elements);
+
+
